50ecbc2d5d28029eacaa272ce5bc2d1e54744cc3
Ahlem Bey <ahbey@f2r10s19.clusters.42paris.fr>
2025-09-23 21:35:09 +0200

    fini

--- cpp_06/ex00/ScalarConverter.cpp
+++ cpp_06/ex00/ScalarConverter.cpp
@@ -1,6 +1,9 @@
 #include "ScalarConverter.hpp"
 #include <cmath>
 
+#include "ScalarConverter.hpp"
+#include <cmath>
+
 ScalarConverter::ScalarConverter()
 {
     // std::cout << "Constructor ScalarConverter has called" << std::endl;
@@ -36,8 +39,8 @@ void ScalarConverter::convert(std::string literal)
         if (literal.length() == 1 && !std::isdigit(literal[0]))
         {
             char c = literal[0];
-            std::cout << "char: '" << c << "'" << std::endl;
             std::cout << "int: " << static_cast<int>(c) << std::endl;
+            std::cout << "char: '" << c << "'" << std::endl;
             std::cout << "float: " << static_cast<float>(c) << "f" << std::endl;
             std::cout << "double: " << static_cast<double>(c) << std::endl;
             return;
@@ -49,43 +52,71 @@ void ScalarConverter::convert(std::string literal)
             if (literal == "nan" || literal == "nanf")
                 std::cout << "float : nanf" << std::endl;
             else if (literal == "-inf" || literal == "+inf" || literal == "-inff" || literal == "+inff")
-                    std::cout << "float : " << literal[0] << "inff" << std::endl;
+                std::cout << "float : " << literal[0] << "inff" << std::endl;
             else if (literal == "inf" || literal == "inff")
-                    std::cout << "float : inff" << std::endl;
+                std::cout << "float : inff" << std::endl;
             else
                 std::cout << "float : impossible" << std::endl;
 
             if (literal == "nan" || literal == "nanf")
                 std::cout << "double : nan" << std::endl;
-            else if (literal == "-inf" || literal == "+inf"  || literal == "-inff" || literal == "+inff")
+            else if (literal == "-inf" || literal == "+inf" || literal == "-inff" || literal == "+inff")
                 std::cout << "double : " << literal[0] << "inf" << std::endl;
             else if (literal == "inf" || literal == "inff")
-                    std::cout << "double : inf" << std::endl;
+                std::cout << "double : inf" << std::endl;
             else
                 std::cout << "double : impossible" << std::endl;
             return;
         }
+
+
+        // Parsing
+        int dotCount = 0;
+        int countF = 0;
+        for (size_t i = 0; i < literal.length(); ++i)
+        {
+            if (literal[i] == '.')
+                dotCount++;
+            if(literal[i] == 'f')
+                countF++;
+        }
+        if (dotCount > 1 || countF > 1)
+        {
+            std::cout << "char : impossible" << std::endl;
+            std::cout << "int : impossible" << std::endl;
+            std::cout << "float : impossible" << std::endl;
+            std::cout << "double : impossible" << std::endl;
+            return;
+        }
+        for (size_t i = 0; i < literal.length(); ++i)
+        {
+            if ((std::isalpha(literal[i]) && (literal.length() != i && literal[i] != 'f'))
+                || (literal[i] == 'f' && literal[i + 1] != '\0'))
+            {
+                std::cout << "char : impossible" << std::endl;
+                std::cout << "int : impossible" << std::endl;
+                std::cout << "float : impossible" << std::endl;
+                std::cout << "double : impossible" << std::endl;
+                return;
+            }
+        }
     }
 
-    
-    
     // INT
     try
     {
         char *end;
-        long result = std::strtol(literal.c_str(), &end, 10);
-
-        // Vérifie que toute la chaîne a été convertie et que la valeur est dans les bornes d'un int
-        if (result > INT_MAX || result < INT_MIN)
+        long resultInt = static_cast<int>(std::strtol(literal.c_str(), &end, 10));
+        if (resultInt > INT_MAX || resultInt < INT_MIN)
         {
             std::cout << "int : impossible" << std::endl;
         }
         else
         {
-            std::cout << "int : " << static_cast<int>(result) << std::endl;
+            std::cout << "int : " << resultInt << std::endl;
         }
     }
-    
+
     catch (std::exception &e)
     {
         std::cout << "int : impossible" << std::endl;
@@ -108,7 +139,7 @@ void ScalarConverter::convert(std::string literal)
     // FLOAT
     try
     {
-        resultFloat = (std::atof(literal.c_str()));
+        resultFloat = static_cast<float>(std::atof(literal.c_str()));
         std::cout << "float : " << resultFloat << ".0f" << std::endl;
     }
     catch (std::exception &e)
@@ -119,7 +150,7 @@ void ScalarConverter::convert(std::string literal)
     // DOUBLE
     try
     {
-        resultDouble = (std::atof(literal.c_str()));
+        resultDouble = static_cast<double>(std::atof(literal.c_str()));
         std::cout << "double : " << resultDouble << ".0" << std::endl;
     }
     catch (std::exception &e)
