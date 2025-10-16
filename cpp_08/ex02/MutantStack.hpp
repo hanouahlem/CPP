53b1fa8bf5693097ff997820bf22cac8b8060853
Ahlem Bey <ahbey@f2r10s19.clusters.42paris.fr>
2025-10-16 20:31:38 +0200

    add ex02

--- cpp_08/ex02/MutantStack.hpp
+++ cpp_08/ex02/MutantStack.hpp
@@ -1,11 +1,43 @@
 #pragma once
 
+#include <stack>
+#include <iostream>
 
 
-class MutantStack
+template<typename T>
+class MutantStack : public std::stack<T>
 {
     private :
-             
-
+    
     public :
-};
+            MutantStack()
+            {}
+            ~MutantStack()
+            {}
+            MutantStack(const MutantStack &other)
+            {
+                std::stack<T>(other);
+            }
+            MutantStack &operator=(const MutantStack &other)
+            {
+                std::stack<T>::operator=(other);
+                return *this;
+            }
+            
+            typedef typename std::stack<T>::container_type::iterator iterator;
+            typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+              
+            iterator begin()
+            {
+                return this->c.begin();
+            }
+            iterator end()
+            {
+                return this->c.end();
+            }
+        };
+// if (elements.empty()) {
+// throw std::out_of_range("Stack is empty!");
+
+
+
