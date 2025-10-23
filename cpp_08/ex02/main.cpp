255d5ab5fbc6395a9db7c1e05bc58980b0777074
Ahlem Bey <ahbey@f2r7s18.clusters.42paris.fr>
2025-10-23 20:48:49 +0200

    fini

--- cpp_08/ex02/main.cpp
+++ cpp_08/ex02/main.cpp
@@ -6,27 +6,37 @@ int main()
     mstack.push(5);
     mstack.push(17);
     mstack.push(7);
+    mstack.push(2);
     std::cout << "Top = " << mstack.top() << std::endl;
+    std::cout << "Size = " << mstack.size() << std::endl;
+
 
     mstack.pop();
-    std::cout << "Size = " << mstack.size() << std::endl;
+
     std::cout << "Top = " << mstack.top() << std::endl;
+    std::cout << "Size = " << mstack.size() << std::endl;
 
     mstack.push(3);
     mstack.push(5);
     mstack.push(737);
     std::cout << "Top = " << mstack.top() << std::endl;
 
+    mstack.push(24);
     mstack.push(0);
     MutantStack<int>::iterator it = mstack.begin();
     MutantStack<int>::iterator ite = mstack.end();
     ++it;
+    std::cout << "++it = " << *it << std::endl;
     --it;
+    std::cout << "--it = " << *it << std::endl;
+
     while (it != ite)
     {
         std::cout << *it << std::endl;
         ++it;
     }
+
     std::stack<int> s(mstack);
+    
     return 0;
 }
