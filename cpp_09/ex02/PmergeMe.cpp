45d072c98c6ebe6aed6f3e8f9c052eff9b6ec1c2
Ahlem Bey <ahbey@f2r11s7.clusters.42paris.fr>
2025-11-20 16:04:40 +0100

    avant dernier push

--- cpp_09/ex02/PmergeMe.cpp
+++ cpp_09/ex02/PmergeMe.cpp
@@ -84,28 +84,6 @@ void PmergeMe::PrintVec()
 
 }
 
-void PmergeMe::SortFourVec(std::vector<int> &vector, int start, int end)
-{
-    int i;
-    int j;
-    int nbr;
-    int tmp;
-
-    i = start + 1;
-    while(i <= end)
-    {
-        nbr = vector[i];
-        j = i -1;
-        while(j >= start && nbr < vector[j])
-        {
-            tmp = vector[j];
-            vector[j] = nbr;
-            vector [j + 1] = tmp;
-            j--;
-        }
-        i++;
-    }
-}
 void PmergeMe::MakePairsVec(std::vector<std::pair<int, int> > &VecPairs)
 {
     int i = 0;
@@ -193,7 +171,7 @@ void PmergeMe::MergeSortPairsVec(std::vector<std::pair<int, int> >& VecPairs, in
         }
     }
 
-    // Si il reste des éléments dans right
+    // Si il reste un éléments dans right
     while (iteratorRight != right.end())
     {
         VecPairs[start].first  = iteratorRight->first;
@@ -349,8 +327,8 @@ void PmergeMe::vectorPrgm()
     PrintVec();
 
     clock_t VecTimeToStart = clock();
-    if(_vector.size() <= 4)
-        SortFourVec(_vector, 0, _vector.size() - 1);
+    if(_vector.size() == 1)
+        PrintVec();
     else
     {
         MakePairsVec(VecPairs);
@@ -359,14 +337,291 @@ void PmergeMe::vectorPrgm()
         InsertJacob(VecPairs, vecTab);
     }
     clock_t VecTimeToEnd = clock();
+
+    // ** Deque ** //
+
+    clock_t DequeTimeToStart = clock();
+
+    std::deque<std::pair<int, int> > DequePairs;
+    std::deque<int> DequeTab;
+    if(_deque.size() == 1)
+        return;
+    else
+    {
+        MakePairsDeque(DequePairs);
+        SortPairsDeque(DequePairs, 0, VecPairs.size() - 1); 
+        InitJacobsthalDeque(DequePairs, DequeTab);
+        InsertJacobDeque(DequePairs, DequeTab);
+    }
+    clock_t DequeTimeToEnd = clock();
+
+
+
     std::cout << "After:   ";
     PrintVec();
     
     double vectorTime = static_cast<double>(VecTimeToEnd - VecTimeToStart) / CLOCKS_PER_SEC * 1000000;
-    // double dequeTime = static_cast<double>(endDeque - startDeque) / CLOCKS_PER_SEC * 1000000;
+    double dequeTime = static_cast<double>(DequeTimeToEnd - DequeTimeToStart) / CLOCKS_PER_SEC * 1000000;
 
     std::cout << "Time to process a range of " << _vector.size()
              << " elements with std::vector : " << vectorTime << " us" << std::endl;
-    // std::cout << "Time to process a range of " << _deque.size()
-            //    << " elements with std::deque : " << dequeTime << " us" << std::endl;
+    std::cout << "Time to process a range of " << _deque.size()
+               << " elements with std::deque : " << dequeTime << " us" << std::endl;
+}
+
+
+
+// ****************************************  DEQUE  ********************************************************
+// ****************************************  DEQUE  ********************************************************
+// ****************************************  DEQUE  ********************************************************
+// ****************************************  DEQUE  ********************************************************
+// ****************************************  DEQUE  ********************************************************
+// ****************************************  DEQUE  ********************************************************
+// ****************************************  DEQUE  ********************************************************
+// ****************************************  DEQUE  ********************************************************
+
+
+
+void PmergeMe::PrintDeque()
+{
+    for (std::deque<int>::const_iterator it = _deque.begin(); it != _deque.end(); ++it) {
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
+
+}
+
+void PmergeMe::MakePairsDeque(std::deque<std::pair<int, int> > &DequePairs)
+{
+    int i = 0;
+    size_t k = 1;
+    size_t len = _deque.size();
+    
+    while(k < len)
+    {
+        int a = _deque[i];
+        int b = _deque[k];
+
+        if (a < b) {
+            DequePairs.push_back(std::make_pair(b, a));
+            // std::cout << "pair indices (" << i << "," << k << ") : (" << b << "," << a << ")" << std::endl;
+        } else {
+            DequePairs.push_back(std::make_pair(a, b));
+            // std::cout << "pair indices (" << i << "," << k << ") : (" << a << "," << b << ")" << std::endl;
+        }
+        i = i + 2;
+        k = k + 2;
+    }
+}
+
+// tri par insertion, appliqué à des paires et basé uniquement sur la valeur .first
+void PmergeMe::SortPairsMaxDeque(std::deque<std::pair<int, int> >& DequePairs,int start, int end)
+{
+    int nbrFirst;
+    int nbrSecond;
+    int k;
+    int tmp;
+    int tmpBis;
+    
+    for (int i = start + 1; i <= end; ++i)
+    {
+        nbrFirst = DequePairs[i].first;
+        nbrSecond = DequePairs[i].second;
+        k = i - 1;
+        while (k >= start && nbrFirst < DequePairs[k].first)
+        {
+            tmp = DequePairs[k].first;
+            tmpBis = DequePairs[k].second;
+            DequePairs[k].first = nbrFirst;
+            DequePairs[k].second = nbrSecond;
+            DequePairs[k + 1].first = tmp;
+            DequePairs[k + 1].second = tmpBis;
+            k--;
+        }
+    }
+}
+
+void PmergeMe::MergeSortPairsDeque(std::deque<std::pair<int, int> >& DequePairs, int start, int middle, int end)
+{
+    std::deque<std::pair<int, int> > left;
+    std::deque<std::pair<int, int> > right;
+
+    left.assign(DequePairs.begin() + start, DequePairs.begin() + middle + 1);
+    right.assign(DequePairs.begin() + middle + 1, DequePairs.begin() + end + 1);
+
+
+    std::deque<std::pair<int, int> >::const_iterator iteratorLeft = left.begin();
+    std::deque<std::pair<int, int> >::const_iterator iteratorRight = right.begin();
+
+    // Fusion
+    while (iteratorLeft != left.end())
+    {
+        if (iteratorRight == right.end())
+        {
+            DequePairs[start].first  = iteratorLeft->first;
+            DequePairs[start].second = iteratorLeft->second;
+            start++;
+            iteratorLeft++;
+        }
+        else if (*iteratorLeft < *iteratorRight)
+        {
+            DequePairs[start].first  = iteratorLeft->first;
+            DequePairs[start].second = iteratorLeft->second;
+            start++;
+            iteratorLeft++;
+        }
+        else
+        {
+            DequePairs[start].first  = iteratorRight->first;
+            DequePairs[start].second = iteratorRight->second;
+            start++;
+            iteratorRight++;
+        }
+    }
+
+    // Si il reste un éléments dans right
+    while (iteratorRight != right.end())
+    {
+        DequePairs[start].first  = iteratorRight->first;
+        DequePairs[start].second = iteratorRight->second;
+        start++;
+        iteratorRight++;
+    }
+
+}
+
+void PmergeMe::SortPairsDeque(std::deque<std::pair<int, int> >& DequePairs, int start, int end)
+{
+    int middle = 0;
+
+    if(start < end)
+    {
+        if(end - start == 1)
+        {
+            SortPairsMaxDeque(DequePairs, start,end);
+        }
+        else
+        {
+            middle = start + ((end - start) / 2);
+            SortPairsDeque(DequePairs, start, middle);
+            SortPairsDeque(DequePairs, middle + 1, end);
+            MergeSortPairsDeque(DequePairs, start, middle, end);
+        }
+    }
+}
+
+void PmergeMe::InitJacobsthalDeque(std::deque<std::pair<int,int> >& DequePairs, std::deque<int>& DequeTab)
+{
+    int n = DequePairs.size();
+    if (n == 0)
+        return;
+
+    std::deque<int> SeqJacob;
+    SeqJacob.push_back(1);
+    // n >= 3 avant d'ajouter 3 sinon on aura des valeurs > n
+    if (n >= 3)
+        SeqJacob.push_back(3);
+
+    // construire la suite jusqu'à <= n
+    while (true)
+    {
+        if (SeqJacob.size() < 2) break; // garde la sûreté
+        int next = SeqJacob[SeqJacob.size()-1] + 2 * SeqJacob[SeqJacob.size()-2];
+        if (next > n)
+            break;
+        SeqJacob.push_back(next);
+    }
+
+    DequeTab.clear();
+
+    // remplir DequeTab à partir de SeqJacob (en veillant à rester <= n)
+    for (size_t i = 0; i < SeqJacob.size(); ++i)
+    {
+        if (SeqJacob[i] <= n)
+            DequeTab.push_back(SeqJacob[i]);
+
+        if (i + 1 < SeqJacob.size())
+        {
+            int start = SeqJacob[i + 1] - 1;
+            int stop  = SeqJacob[i];
+            if (start > n) start = n;
+            int s = start;
+            while (s > stop)
+            {
+                if (s <= n)
+                    DequeTab.push_back(s);
+                s--;
+            }
+        }
+    }
+
+    // Ajouter les indices manquants (1..n) dans l'ordre décroissant comme avant
+    for (int k = n; k >= 1; --k)
+    {
+        if (std::find(DequeTab.begin(), DequeTab.end(), k) == DequeTab.end())
+            DequeTab.push_back(k);
+    }
+
+}
+
+int PmergeMe::SearchWereIsertDeque(std::deque<int>& FinalDeque, int start, int end, int target) 
+{
+    if (FinalDeque.empty())
+        return 0;
+    int middle = 0;
+
+    while(end - start > 1)
+    {
+        middle = start + ((end - start) / 2);
+        if (target > FinalDeque[middle])
+            start = middle +1; 
+        if (target < FinalDeque[middle])
+            end = middle;         
+    }
+    if (target > FinalDeque[start])
+        start++;
+    return start;
+}
+
+void PmergeMe::InsertJacobDeque(std::deque<std::pair<int,int> >& DequePairs,std::deque<int>& DequeTab)
+{
+    std::deque<int> FinalDeque;  
+    size_t i = 0;
+    int NbrInserted = 0; 
+    int lastOdd = 0;
+
+    // Si le nombre d'éléments initial est impair, on conserve le dernier élément
+    if (_deque.size() % 2 != 0)
+        lastOdd = _deque.back();
+
+    // On commence par remplir FinalDeque avec les plus grands de chaque paire
+    for (size_t i = 0; i < DequePairs.size(); ++i)
+        FinalDeque.push_back(DequePairs[i].first);
+
+    // Insérer le premier “petit” élément b0
+    FinalDeque.insert(FinalDeque.begin(), DequePairs[DequeTab[i]-1].second);
+    NbrInserted++;
+    i++;
+
+    // Boucle d’insertion selon l’ordre Jacobsthal
+    while (i < DequeTab.size())
+    {
+        int target = DequePairs[DequeTab[i] - 1].second;
+        int pos = SearchWereIsertDeque(FinalDeque, 0, DequeTab[i] + NbrInserted - 1, target);
+        FinalDeque.insert(FinalDeque.begin() + pos, target);
+        NbrInserted++;
+        i++;
+    }
+
+    // Si taille initiale impair, insérer le dernier élément
+    if (_deque.size() % 2 != 0)
+    {
+        int pos = SearchWereIsertDeque(FinalDeque, 0, FinalDeque.size() - 1, lastOdd);
+        if (pos >= (int)FinalDeque.size())
+            FinalDeque.push_back(lastOdd);
+        else
+            FinalDeque.insert(FinalDeque.begin() + pos, lastOdd);
+    }
+
+    _deque = FinalDeque;
 }
--- cpp_09/ex02/PmergeMe.hpp
+++ cpp_09/ex02/PmergeMe.hpp
@@ -19,18 +19,28 @@ class PmergeMe
             int checkInt(long resConvert);
             int isValidNumber(const std::string &str);
             int pushIfGood(double resConvert);
-            void printNumber();
 
             void PrintVec();
             void vectorPrgm();
-            void SortFourVec(std::vector<int> &vector, int start, int end);
+    
             void MakePairsVec(std::vector<std::pair<int, int> > &myPairs);
             void SortPairsVec(std::vector<std::pair<int, int> >& VecPairs, int start, int end);
             void SortPairsMax(std::vector<std::pair<int, int> >& myPairs,int start, int end);
             void MergeSortPairsVec(std::vector<std::pair<int, int> >& VecPairs, int start, int middle, int end);
-            // void InitJacobsthal(std::vector<std::pair<int,int> >& VecPairs, std::vector<int>& VecTab);
+            void InitJacobsthal(std::vector<std::pair<int,int> >& VecPairs, std::vector<int>& VecTab);
             void InsertJacob(std::vector<std::pair<int, int> > &VecPairs, std::vector<int>& VecTab);
             int BinarySearch(std::vector<int>& mainChain, int start, int end, int target);
-            void InitJacobsthal(std::vector<std::pair<int, int> > &myPairs, std::vector<int>& jacobjacob) ;
+
+            void MakePairsDeque(std::deque<std::pair<int, int> > &myPairs);
+            void SortPairsDeque(std::deque<std::pair<int, int> >& DequePairs, int start, int end);
+            void SortPairsMaxDeque(std::deque<std::pair<int, int> >& myPairs,int start, int end);
+            void MergeSortPairsDeque(std::deque<std::pair<int, int> >& DequePairs, int start, int middle, int end);
+            void InitJacobsthalDeque(std::deque<std::pair<int,int> >& DequePairs, std::deque<int>& DequeTab);
+            void InsertJacobDeque(std::deque<std::pair<int, int> > &DequePairs, std::deque<int>& DequeTab);
+            int SearchWereIsertDeque(std::deque<int>& mainChain, int start, int end, int target);
+            void PrintDeque();
+
+
+
 
 };
